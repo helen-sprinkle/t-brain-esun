1012c3b46a5eb517dc623fa74438a3699ef1065e
Helen Chang <helensprinkling@gmail.com>
2019-02-24 14:33:15 +0800

    initial commit: project start

---
+++ dirs.R
@@ -0,0 +1,17 @@
+# config directory paths and other options
+# [wd] working directory
+# [src] source directory
+# [aux.dir] auxiliary directory
+# [data.root] where data should be 
+# [data.dir] data read-in directory
+
+wd <- "."
+src <- ifelse(grepl("[Hh]elen", Sys.info()["nodename"]), wd, "src")
+aux.dir <- "../auxiliary"
+data.root <- file.path("../../data", basename(getwd()))
+data.dir <- file.path(data.root, "data")
+
+stopifnot(dir.exists(src) & dir.exists(aux.dir))
+
+create.proj(research=T,training=T,production=F,portable=F)
+options(scipen=999, digits = 9, stringsAsFactors = F)
