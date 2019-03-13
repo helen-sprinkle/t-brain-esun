cef8614acc0c6f9f51c216fd5bcdb19a46a5094a
Helen Chang <helensprinkling@gmail.com>
2019-03-13 23:19:38 +0800

    minor change adapt with new kind of feature (bencode and recent_wayback)

--- view.results.R
+++ view.results.R
@@ -1,3 +1,9 @@
+source("dirs.R")
+source(file.path(aux.dir, "mylib.R"))
+source(file.path(aux.dir, "slackme.R"))
+mylib(c("data.table"))
+
+
 fls <- list.files(file.path(data.dir, "output"))
 
 dt.ls <- lapply(fls, function(fnm) {
@@ -7,6 +13,7 @@ dt.ls <- lapply(fls, function(fnm) {
 dt <- rbindlist(dt.ls)
 setnames(dt, colnames(dt), c("round","learning_rate","max_depth","mds","sb_sample",
                              "spw","auc","Fmeasure","precision","recall","fnm"))
-dt[, c("x", "y", "n") := tstrsplit(fnm,"_",keep=c(3,4,5))]
-write.table(dt, file="summary_and_reports/results.csv",row.names = F)
-            # col.names = F, append = T, sep = ",")
+dt[, fnm:=gsub("ben_recent_wayback", "ben.recent.wayback", dt$fnm)]
+dt[, c("x", "y", "n") := tstrsplit(fnm,"_",keep=c(4,5,2))]
+write.table(dt, file="summary_and_reports/results.csv",row.names = F, sep = ",",
+            col.names = F, append = T)
